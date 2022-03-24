fb0fb0aa77d7f677a165f0385ebb4ac0f870468e
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-24 17:24:51 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: Marshall Demars
+// Created on: Mar 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+  public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string baseTriangle;
+        int heightTriangle;
+
+        Console.WriteLine("This program finds the area of a triangle");
+        Console.WriteLine("");
+
+        Console.Write("Enter the height: ");
+        heightTriangle = Console.ReadLine();
+        Console.Write("Enter the base: ");
+        heightBase = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("The area is: " + (heightTriangle * baseTriangle) / 2 + " " + ".");
+
+        Console.WriteLine("\nDone.");
+    }
+}
