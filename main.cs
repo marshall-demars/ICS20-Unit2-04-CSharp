c204c116c8073665edbbba8648b01ffdb543ce19
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-24 23:57:49 +0000

    3 files

--- main.cs
+++ main.cs
@@ -7,7 +7,7 @@ using System;
 
 class Program
 {
-  public static void Main(int[] args)
+  public static void Main(string[] args)
     {
         // This function accepts user input
         int baseTriangle;
